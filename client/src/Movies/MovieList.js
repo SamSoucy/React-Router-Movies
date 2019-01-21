611e09313d5fd50817bcbdb15788a6441ce5d8c7
Samsoucy <newf930@outlook.com>
2019-01-21 18:23:05 -0500

    fixed link to individual movies

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -35,23 +35,23 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <Link to={`/movies/${this.state.movie.id}`}>
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
+    <div className="movie-card">
+      <h2>
+        <Link to={`/movies/${movie.id}`}> {title}</Link>
+      </h2>
+      <div className="movie-director">
+        Director: <em>{director}</em>
       </div>
-    </Link>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
   );
 }
